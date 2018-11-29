8fa876a467bfd968c15bca610b1812668567498d
Xavi Ramirez <xavi.ramirez@clever.com>
2018-11-29 12:53:41 -0800

    Only wait when looking for new task

--- cmd/sfncli/sfncli.go
+++ cmd/sfncli/sfncli.go
@@ -121,13 +121,13 @@ func main() {
 	// getactivitytask claims to initiate a polling loop, but it seems to return every few minutes with
 	// a nil error and empty output. So wrap it in a polling loop of our own
 	for mainCtx.Err() == nil {
-		if err := limiter.Wait(mainCtx); err != nil {
-			continue
-		}
 		select {
 		case <-mainCtx.Done():
 			log.Info("getactivitytask-stop")
 		default:
+			if err := limiter.Wait(mainCtx); err != nil {
+				continue
+			}
 			cw.SetActiveState(false)
 			log.TraceD("getactivitytask-start", logger.M{"activity-arn": *createOutput.ActivityArn, "worker-name": *workerName})
 			getATOutput, err := sfnapi.GetActivityTaskWithContext(mainCtx, &sfn.GetActivityTaskInput{
